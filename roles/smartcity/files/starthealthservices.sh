b00442b319192bc575dbe42cdadc39faf8dcd893
Chase Carthen <ccarthen@unr.edu>
2022-06-27 15:34:00 -0700

    adding a script to start health services

---
+++ roles/smartcity/files/starthealthservices.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export HOST_IP=$1
+export DIR=$(dirname $0)
+echo ${HOST_IP}
+echo $2
+if [[ $2 == "windows" ]]
+then
+        echo "windows"
+        export BASE_PATH=/c/Users/edge/Documents/infrastructure
+else
+	export HOST_IP=$(hostname -I | awk '{print $1}')
+        export BASE_PATH=/home/edge/infrastructure
+fi
+
+
+docker-compose -f $DIR/docker-compose-infrastructure.yml  up -d --remove-orphans cadvisor consul registrator filebeat filebeatnetflow node-exporter
